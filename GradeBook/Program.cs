e1a1db0341b39dcad31f8cfadd7c97b35f6e2003
James.Remus <James.Remus@VUHL-H0HJKN3>
2022-11-12 15:33:52 -0600

    Added try catch for dictionary

--- GradeBook/Program.cs
+++ GradeBook/Program.cs
@@ -9,80 +9,95 @@ namespace GradeBook
         {
             decimal classAverage = 0;
             string studentLetterGrade = null;
+            try
+            {
+                IDictionary<string, Dictionary<string, List<decimal>>> Students = new Dictionary<string, Dictionary<string, List<decimal>>>
+                {
 
-            IDictionary<string, Dictionary<string, List<decimal>>> Students = new Dictionary<string, Dictionary<string, List<decimal>>>
-            {
-                {
-                    "Morty",
-                    new Dictionary<string, List<decimal>>
                     {
-                        { "Homework", new List<decimal> { 15, 20, 18, 19 } },
-                        { "Quizzes", new List<decimal> { 45, 48, 42 } },
-                        { "Tests", new List<decimal> { 92, 98 } }
-                    }
-                },
-                {
-                    "Charlie",
-                    new Dictionary<string, List<decimal>>
-                    {
-                        { "Homework", new List<decimal> { 14, 16, 18, 17 } },
-                        { "Quizzes", new List<decimal> { 42, 39, 36 } },
-                        { "Tests", new List<decimal> { 86, 78 } }
-                    }
-                },
-                {
-                    "Opie",
-                    new Dictionary<string, List<decimal>>
-                    {
-                        { "Homework", new List<decimal> { 19, 20, 18, 17 } },
-                        { "Quizzes", new List<decimal> { 47, 47, 50 } },
-                        { "Tests", new List<decimal> { 97, 95 }
-                        }
-                    }
-                }
-            };
-            void PrintStudentInfo(IDictionary<string, Dictionary<string, List<decimal>>> students)
-            {
-                foreach (var student in students)
-                {
-                    List<decimal> studentAverageScores = new List<decimal>();
-                    decimal studentClassTotal = 0;
-                    foreach (var item in student.Value)
-                    {
-                        
-                        decimal scoreAverage = 0;
-                        Console.WriteLine("{0} ->> {1}", student.Key, item.Key);
-                        foreach (var grade in item.Value)
+                        "James",
+                        new Dictionary<string, List<decimal>>
                         {
-                            Console.Write("{0} ", grade);
-                            studentClassTotal += grade;
+                            { "Homework", new List<decimal> { 15, 20, 18, 19 } },
+                            { "Quizzes", new List<decimal> { 45, 48, 42 } },
+                            { "Tests", new List<decimal> { 92, 98 } }
                         }
-                        scoreAverage = CalculateAssignmentAverage(item.Value);
-                        studentAverageScores.Add(scoreAverage);
-                        Console.WriteLine("\n{0}'s average of the {1} is {2}.", student.Key, item.Key, scoreAverage);
-                        Console.WriteLine();
+                    },
+                    {
+                        "Morty",
+                        new Dictionary<string, List<decimal>>
+                        {
+                            { "Homework", new List<decimal> { 15, 20, 18, 19 } },
+                            { "Quizzes", new List<decimal> { 45, 48, 42 } },
+                            { "Tests", new List<decimal> { 92, 98 } }
+                        }
+                    },
+                    {
+                        "Charlie",
+                        new Dictionary<string, List<decimal>>
+                        {
+                            { "Homework", new List<decimal> { 14, 16, 18, 17 } },
+                            { "Quizzes", new List<decimal> { 42, 39, 36 } },
+                            { "Tests", new List<decimal> { 86, 78 } }
+                        }
+                    },
+                    {
+                        "Opie",
+                        new Dictionary<string, List<decimal>>
+                        {
+                            { "Homework", new List<decimal> { 19, 20, 18, 17 } },
+                            { "Quizzes", new List<decimal> { 47, 47, 50 } },
+                            { "Tests", new List<decimal> { 97, 95 }
+                            }
+                        }
                     }
-                    
-                    classAverage = CalculateClassAverage(studentClassTotal);
-                    classAverage = Decimal.Round(classAverage, 2);
-                    studentLetterGrade = GetLetterGrade(classAverage*100);
-                    Console.WriteLine("{0}'s class average is {1}%.", student.Key, classAverage*100);
-                    Console.WriteLine("{0}'s grade in the class is {1}.\n", student.Key, studentLetterGrade);
                 };
-            }
-            PrintStudentInfo(Students);
+                void PrintStudentInfo(IDictionary<string, Dictionary<string, List<decimal>>> students)
+                {
+                    foreach (var student in students)
+                    {
+                        List<decimal> studentAverageScores = new List<decimal>();
+                        decimal studentClassTotal = 0;
+                        foreach (var item in student.Value)
+                        {
 
-            decimal CalculateAssignmentAverage (List<decimal> scores)
+                            decimal scoreAverage = 0;
+                            Console.WriteLine("{0} ->> {1}", student.Key, item.Key);
+                            foreach (var grade in item.Value)
+                            {
+                                Console.Write("{0} ", grade);
+                                studentClassTotal += grade;
+                            }
+                            scoreAverage = CalculateAssignmentAverage(item.Value);
+                            studentAverageScores.Add(scoreAverage);
+                            Console.WriteLine("\n{0}'s average of the {1} is {2}.", student.Key, item.Key, scoreAverage);
+                            Console.WriteLine();
+                        }
+
+                        classAverage = CalculateClassAverage(studentClassTotal);
+                        classAverage = Decimal.Round(classAverage, 2);
+                        studentLetterGrade = GetLetterGrade(classAverage * 100);
+                        Console.WriteLine("{0}'s class average is {1}%.", student.Key, classAverage * 100);
+                        Console.WriteLine("{0}'s grade in the class is {1}.\n", student.Key, studentLetterGrade);
+                    };
+                }
+                PrintStudentInfo(Students);
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("You have entered a name that has already been used!");
+            };
+            decimal CalculateAssignmentAverage(List<decimal> scores)
             {
                 decimal sumScores = 0;
                 foreach (var score in scores)
                 {
-                   sumScores = sumScores + score;
+                    sumScores = sumScores + score;
                 }
                 decimal averageScores = sumScores / scores.Count;
-                return averageScores; 
+                return averageScores;
             }
-            decimal CalculateClassAverage (decimal studentTotalScores)
+            decimal CalculateClassAverage(decimal studentTotalScores)
             {
                 decimal studentClassAverage = studentTotalScores / 430;
                 return studentClassAverage;

e52395b2df7c077b25b028765583692274eabfb5
James.Remus <James.Remus@VUHL-H0HJKN3>
2022-11-16 15:33:04 -0600

    Exercise 1 integer list

---
+++ Week10Homework/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Week10Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> integerList = new List<int>();
+            string userInput = null;
+            int sumIntegerList = 0;
+            int averageIntegerList = 0;
+            do
+            {
+                Console.WriteLine("Please enter a positive integer:");
+                userInput = Console.ReadLine();
+                try
+                {
+                    int enteredInteger = int.Parse(userInput);
+                    if (enteredInteger < 0)
+                    {
+                        Console.WriteLine("You did not enter a positive integer. Please try again!");
+                        continue;
+                    }
+                    integerList.Add(enteredInteger);
+                    sumIntegerList += enteredInteger;
+                    averageIntegerList = sumIntegerList / integerList.Count;
+                }
+                
+                catch (System.FormatException)
+                {
+                    Console.WriteLine("You did not enter a number so the program will now exit!");
+                    Environment.Exit(1);
+                };
+                Console.WriteLine("You have entered {0} numbers so far.", integerList.Count);
+                Console.WriteLine("The total of the numbers you have entered is {0}", sumIntegerList);
+                Console.WriteLine("The average of the numbers you have entered is: {0}", averageIntegerList);
+
+                continue;
+            } while (userInput != null);
+            
+        }
+    }
+}
